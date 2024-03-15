0f97095ed87dcb86ffd31f61fa2ae5de26bb3ba5
MarinaV27 <Vasilishinamari@gmail.com>
2024-03-15 21:01:14 +0200

    final task 8

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,6 @@
 const images = [
  {
-    id:1,
+    
      preview:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
      original:
@@ -8,7 +8,7 @@ const images = [
      description: 'Hokkaido Flower',
  },
  {
-    id:2,
+   
      preview:
       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
      original:
@@ -16,7 +16,7 @@ const images = [
      description: 'Container Haulage Freight',
  },
  {
-    id:3,
+    
      preview:
       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
      original:
@@ -24,7 +24,7 @@ const images = [
      description: 'Aerial Beach View',
  },
  {
-    id:4,
+   
      preview:
       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
      original:
@@ -32,7 +32,7 @@ const images = [
      description: 'Flower Blooms',
  },
  {
-    id:5,
+    
      preview:
       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
      original:
@@ -40,7 +40,7 @@ const images = [
      description: 'Alpine Mountains',
  },
  {
-    id:6,
+    
      preview:
      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
      original:
@@ -48,7 +48,7 @@ const images = [
      description: 'Mountain Lake Sailing',
  },
  {
-    id:7,
+    
      preview:
      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
      original:
@@ -64,7 +64,7 @@ const images = [
     description: 'Nature Landscape',
  },
  {
-    id:9,
+   
     preview:
     'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
     original:
@@ -73,53 +73,49 @@ const images = [
  },
     ]    
 
-    const container = document.querySelector("ul.gallery");
+    const container = document.querySelector(".gallery");
+    const img = document.querySelectorAll(".galleri-link")
     container.innerHTML = creatMarkup(images);
     container.addEventListener("click", handleModalOpen);
 
+   
+    function creatMarkup (arr){
+      return arr.map(({preview, original, description}) => 
+      `<li class="gallery-item" >
+       <a class="gallery-link" href="${original}">
+         <img
+           class="gallery-image"
+           src="${preview}"
+           data-source="${original}"
+           alt="${description}"
+           width = 360 px
+           height = 200 px
+         />
+       </a>   
+      </li>`
+      )
+      .join("");
+  }
 
     function handleModalOpen(event){
         if(event.currentTarget === event.target) return;
         event.preventDefault();
-    const currentImage = event.target.closest(".gallery-item");
-    const imageId = Number(currentImage.dataset.id);
-    const image = images.find(({id}) => id = imageId);
-
-    
-    
+    const currentImage = event.target.dataset.source;
+    const image = images.find(({original}) => original === currentImage);
+ 
     const instance = basicLightbox.create( 
-        `<div class="modal">
-        <a class="gallery-link" href="${image.original}">
-          <img
+       
+          `<img
             class="gallery-image"
-            src="${image.preview}"
-            data-source="${image.original}"
-            alt="${image.description}"
-          />
-        </a>   
-       </div>`
-    ) 
+            src="${image.original}"
+            alt="${image.description}" 
+          />`
+    ) ;
     instance.show();
 
     }
 
-    function creatMarkup (arr){
-        return arr.map(({id, preview, original, description}) => 
-        `<li class="gallery-item" data-id="${id}">
-         <a class="gallery-link" href="${original}">
-           <img
-             class="gallery-image"
-             src="${preview}"
-             data-source="${original}"
-             alt="${description}"
-           />
-         </a>   
-        </li>`
-        )
-        .join("");
-    }
-
-
+    
 
     
 
